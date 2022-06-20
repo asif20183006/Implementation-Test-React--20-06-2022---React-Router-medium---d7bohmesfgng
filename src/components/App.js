a3d4a5596338d7deca11ae9d98c888b953558f04
MD ASIF ULLAH <58297854+asif20183006@users.noreply.github.com>
2022-06-20 22:02:53 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom';
 import { AppRoutes } from '../AppRoutes';
-
 import '../styles/App.css';
 const App = () => {
 
   return (
-    <BrowserRouter>
+    <>
+    <div>
       <AppRoutes />
-    </BrowserRouter>
-
+    </div>
+</>
   )
 }
 
