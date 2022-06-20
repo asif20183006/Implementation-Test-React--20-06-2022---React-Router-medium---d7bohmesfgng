6abf2f0d356876a8c5f90c9b73076a75afc5664a
MD ASIF ULLAH <58297854+asif20183006@users.noreply.github.com>
2022-06-21 00:37:04 +0530

    Update Calculator.js

--- src/Pages/Calculator.js
+++ src/Pages/Calculator.js
@@ -7,34 +7,34 @@ export const Calculator = () => {
   var num2 = Number(url.searchParams.get("num2"));
   var op = url.searchParams.get("op");
   console.log(num1, num2, op);
-  let ans = 0;
+  let shu = 0;
   console.log(op);
   switch (op) {
     case "+": {
-      ans = num1 + num2;
+      shu = num1 + num2;
       break;
     }
     case "/": {
-      ans = num1 / num2;
+      shu = num1 / num2;
       break;
     }
     case "-": {
-      ans = num1 - num2;
+      shu = num1 - num2;
       break;
     }
     case "*": {
-      ans = num1 * num2;
+      shu = num1 * num2;
       break;
     }
     case "%": {
-      ans = num1 % num2;
+      shu= num1 % num2;
       break;
     }
   }
 
   return (
     <div>
-      Your calculation Result is <span id="calc-result">{ans}</span>
+      Your calculation Result is <span id="calc-result">{shu}</span>
     </div>
   );
-};
+}
